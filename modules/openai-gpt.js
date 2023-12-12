6630ebcdda0f3ad62d0f97935a88ef79f7d32459
kalpan ig <alart747@gmail.com>
2023-12-12 06:26:13 +0100

    added trycatch to ai request

--- modules/openai-gpt.js
+++ modules/openai-gpt.js
@@ -4,10 +4,15 @@ const openai = new OpenAI({ apiKey: key });
 
 
 export async function main(msg) {
-    const completion = await openai.chat.completions.create({
+    try {
+      const completion = await openai.chat.completions.create({
         messages: [{ role: "user", content: msg }],
         model: "gpt-3.5-turbo",
       });
+    } catch (error) {
+      console.log(error)
+      return 0
+    }
 
       return completion.choices[0].message.content
 }
